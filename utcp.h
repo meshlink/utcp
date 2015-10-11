d68ca08452149ecf25abe770321d82bcdb5bd667
Guus Sliepen <guus@meshlink.io>
2015-10-11 16:31:59 +0200

    Add a function to check for active connections.
    If a connection sends data in one way, then the receiver will have shut
    down data in the other way, and when the sender is finished he will also
    shut down his direction, so the connection looks closed to the sender,
    but the receiver might actually still miss the final packets. So UTCP
    should keep running until the receiver has received a FINACK and is in the
    TIME_WAIT state.
    
    We consider UTCP to be active when there is at least one connection not in
    the CLOSED or TIME_WAIT state.
    
    The test program now uses this condition, which allows a transfer of a file
    to complete without missing the last few bytes.

--- utcp.h
+++ utcp.h
@@ -61,6 +61,7 @@ extern struct timeval utcp_timeout(struct utcp *utcp);
 extern void utcp_set_recv_cb(struct utcp_connection *connection, utcp_recv_t recv);
 extern void utcp_set_poll_cb(struct utcp_connection *connection, utcp_poll_t poll);
 extern void utcp_set_accept_cb(struct utcp *utcp, utcp_accept_t accept, utcp_pre_accept_t pre_accept);
+extern bool utcp_is_active(struct utcp *utcp);
 
 // Global socket options
 
