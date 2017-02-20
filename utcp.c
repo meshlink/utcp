1b17fef706b6a8ea3ebd4c873995e75626440bb0
everHannes <hannes.ahrens@everbase.net>
2017-02-20 13:20:09 +0100

    break connection polling on UTCP_WOULDBLOCK

--- utcp.c
+++ utcp.c
@@ -1866,7 +1866,7 @@ struct timeval utcp_timeout(struct utcp *utcp) {
             if(c->reapable) {
                 debug("Reaping %p\n", c);
                 free_connection(c);
-                i--;
+                --i;
             }
             continue;
         }
@@ -1892,7 +1892,9 @@ struct timeval utcp_timeout(struct utcp *utcp) {
             struct timeval retry = {0,1000};
             if(timercmp(&retry, &next, <))
                 next = retry;
-            continue;
+
+            // break on UTCP_WOULDBLOCK to proceed with the next connection next time
+            break;
         }
 
         // when there's nothing pending queued, check the retransmit timeout
@@ -1923,19 +1925,29 @@ struct timeval utcp_timeout(struct utcp *utcp) {
             // try to send any remainining buffered data
             // the polling might only call utcp_send and ack when there's something new to send
             // on error return with a 1ms timeout to retry soon
-            if(0 != ack(c, false)) {
+            int err = ack(c, false);
+            if(0 != err) {
                 struct timeval retry = {0,1000};
                 if(timercmp(&retry, &next, <))
                     next = retry;
+
+                // break on UTCP_WOULDBLOCK to proceed with the next connection next time
+                if(UTCP_WOULDBLOCK == err)
+                    break;
             }
         }
         // also retry the last shutdown send if failed
         else if(c->state == FIN_WAIT_1 || c->state == CLOSING) {
             // on error return with a 1ms timeout to retry soon
-            if(0 != ack(c, false)) {
+            int err = ack(c, false)) {
+            if(0 != err) {
                 struct timeval retry = {0,1000};
                 if(timercmp(&retry, &next, <))
                     next = retry;
+
+                // break on UTCP_WOULDBLOCK to proceed with the next connection next time
+                if(UTCP_WOULDBLOCK == err)
+                    break;
             }
         }
     }
